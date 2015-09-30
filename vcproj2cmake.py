cd1bb10f766b1101edc0708d68a9caeb4859883f
keszegrobert <keszegrobert@gmail.com>
2015-09-30 20:44:19 +0200

    added configs and targets

--- vcproj2cmake.py
+++ vcproj2cmake.py
@@ -1,47 +1,85 @@
 import sys
+import os.path
 import xml.etree.ElementTree as ET
+
 tree = ET.parse(sys.argv[1])
 root = tree.getroot()
-print root.tag;
 projname = root.get("Name")
 print "Project name:"+ projname;
 
 print "------ Configurations -----"
-cfgset = set()
+
+targetset = set()
+configset = set()
+platformset = set()
 for config in root.iter("Configuration"):
 	cfgarr = config.get("Name").split("|");
-	cfgname = cfgarr[0]
-	print cfgname
-	cfgset.add(cfgname)
+	cfgname = '|'.join(cfgarr)
+	targetset.add(cfgname)
+	configset.add(cfgarr[0])
+	platformset.add(cfgarr[1])
+	
+#	cfgfilename = '_'.join(cfgarr)
+#	cfgfilename = cfgfilename.replace(" ","_")
+#	print cfgfilename
+#	cfgfile = open(cfgfilename+".xml", "w")
+#	cfgfile.write(ET.tostring(config))
+#	cfgfile.close()
 
-cfg = sorted(list(cfgset))
-print "------ Filters -----"
-cmake_groups = list()
+cfg = sorted(list(configset))
+
+def normalizestring(input):
+	return input.replace("|","_").replace("(","").replace(")","").replace(" ","_").upper()
+
+targetlist = sorted(list(targetset))
+for target in targetlist:
+	print normalizestring(target)
 
 class Group:
 	def __init__(self,names,files):
 		self.names = names
 		normalized = []
 		for file in files:
-			patharr = file.split('\\')[1:]
-			newname = '/'.join(patharr)
+			patharr = file.split('\\')
+			if patharr[0] == '.':
+				newname = '/'.join(patharr[1:])
+			else:
+				newname = '/'.join(patharr)
 			normalized.append(newname)
 		self.files = normalized
+
+	def getname(self):
+		name = projname+'_'+'_'.join(self.names)+'_SRC'
+		return name
+
+	def getnamevar(self):
+		name = '${'+self.getname()+'}'
+		return name
 		
 	def tostring(self):
-		name = projname+'_'+'_'.join(self.names)+'_SRC'
-		setting = '\nset('+name+'\n\t'+'\n\t'.join(self.files)+'\n)\n'
-		group ='source_group("'+'\\\\'.join(self.names)+'" FILES ${'+name+'})\n'
-		return setting + group
+		lines = []
+		lines.append('set('+self.getname())
+		for file in self.files:
+			lines.append('\t'+file)
+		lines.append(')')
 		
+		lines.append('source_group("'+'\\\\'.join(self.names)+'" FILES '+self.getnamevar()+')')
+		return '\n'.join(lines)
+
+print "------ Filters -----"
+cmake_groups = list()
+
 def getgroupsfor(rootnames,lroot):
-	print "//".join(rootnames)
+#	print "//".join(rootnames)
 	files = list()
 	for xmlFile in lroot:
 		if xmlFile.tag == "Filter":
 			continue
 		xmlfilename = xmlFile.get("RelativePath")
-		files.append(xmlfilename)
+		if os.path.exists(xmlfilename):
+			files.append(xmlfilename)
+		else:
+			print "file mapped in vcproj not found: " + xmlfilename;
 	group = Group(rootnames,files)
 	cmake_groups.append(group)
 	
@@ -57,12 +95,37 @@ xmlFiles = root.find("Files")
 getgroupsfor([],xmlFiles)
 
 print "------ Output -----"
-cmakefile = open("CMakeLists.txt","w")
-	
-cmakefile.write('set(CMAKE_CONFIGURATION_TYPES "'+";".join(cfg)+'" CACHE STRING "VS/XCode configurations" FORCE)\n')
-cmakefile.write('cmake_minimum_required(VERSION 2.8)\n');
-cmakefile.write('project('+projname+')\n')
+lines = []
+
+#lines.append('set(CMAKE_CONFIGURATION_TYPES "'+";".join(cfg)+'" CACHE STRING "VS/XCode configurations" FORCE)')
+lines.append('cmake_minimum_required(VERSION 2.8)')
+lines.append('project('+projname+')')
 
 for group in cmake_groups:
-	cmakefile.write(group.tostring())
+	lines.append(group.tostring())
+	
+lines.append('set(SOURCES')
+for group in cmake_groups:
+	lines.append('\t'+group.getnamevar()+'')
+lines.append(')')
+
+lines.append('set(TARGETS')
+for target in targetlist:
+	lines.append('\t'+target)
+for target in targetlist:
+	lines.append('\t'+target)
+lines.append(')')
+
+lines.append('if (NOT CMAKE_BUILD_TYPE)')
+lines.append('set(CMAKE_BUILD_TYPE Debug|Win32 CACHE STRING')
+lines.append(' "Choose the type of the build, options are: ${TARGETS} " FORCE)')
+lines.append('endif (NOT CMAKE_BUILD_TYPE)')
+
+lines.append('add_library( '+projname+' STATIC ${SOURCES} )')
+lines.append('set_target_properties( '+projname+' PROPERTIES LINKER_LANGUAGE C++ )')
+
 print "------ End -----"
+
+cmakefile = open("CMakeLists.txt","w")
+cmakefile.write('\n'.join(lines))
+cmakefile.close()
